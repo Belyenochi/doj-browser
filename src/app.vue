72d1cb75aa7de1688af7116fe87f0c0911ed91d1
ChouUn <chouunsoft@gmail.com>
2017-07-19 02:11:37 +0800

    [build] 初次提交

---
+++ src/app.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div style="width: 700px; height: 300px; margin: auto; position: relative;">
+      <loader></loader>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Loader from '@/components/loader'
+
+export default {
+  name: 'App',
+  components: {
+    Loader,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/loader.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="loader">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+}
+</script>
+
+<style lang="less">
+@import '../styles/index';
+
+@loaderTopOffset: 50%;
+@loaderLeftOffset: 50%;
+*, :after, :before {
+    box-sizing: inherit;
+}
+.loader {
+  position: absolute;
+  top: @loaderTopOffset;
+  left: @loaderLeftOffset;
+  margin: 0px;
+  text-align: center;
+  z-index: 1000;
+  transform: translateX(-50%) translateY(-50%);
+}
+
+.ui.loader:before {
+  position: absolute;
+  content: '';
+  top: 0%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+
+  border-radius: 500rem;
+  border: @loaderLineWidth solid @loaderFillColor;
+}
+
+.loader:after {
+  position: absolute;
+  content: '';
+  top: 0%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+
+  border-radius: 500rem;
+
+  border-color: @loaderLineColor @loaderFillColor @loaderFillColor;
+  border-style: solid;
+  border-width: @loaderLineWidth;
+
+  animation: loader @loaderSpeed linear;
+  animation-iteration-count: infinite; 
+
+  box-shadow: 0px 0px 0px 1px transparent;
+}
+
+@keyframes loader {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@emSize   : 14px;
+@fontSize : 14px;
+@mediumSize      : (32 / 14);
+
+@medium: unit( round(@mediumSize * @emSize) / @emSize, rem);
+@relativeMedium  : unit( round(@mediumSize * @emSize) / @emSize, em);
+@mediumFontSize: @relativeMedium;
+@mediumOffset: 0em 0em 0em -(@medium / 2);
+
+.loader {
+  width: @medium;
+  height: @medium;
+  font-size: @mediumFontSize;
+}
+
+.loader:after {
+  width: @medium;
+  height: @medium;
+  margin: @mediumOffset;
+}
+
+</style>
