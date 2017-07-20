b49b6119ef70fb294697d357bd4a3977830568ca
ChouUn <chouunsoft@gmail.com>
2017-07-20 17:03:08 +0800

    [eslint] 格式化了 dynamic-load 和 material-colors

--- src/styles/utils/dynamic-load.js
+++ src/styles/utils/dynamic-load.js
@@ -1,11 +1,8 @@
 
-function dynamicLoad(url, callback) {
-    if (!url.startsWith('//') && url.startsWith('/')) {
-        url = STATIC_HOST + url
-    }
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    document.head.appendChild(script);
-    script.onload = callback;
-    script.src = url
+export default function dynamicLoad (url, callback) {
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  document.head.appendChild(script)
+  script.onload = callback
+  script.src = url
 }
